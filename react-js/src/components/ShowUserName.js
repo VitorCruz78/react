f26c853b2af253f5801d9b201f9791bb89176be5
Vitor Luiz da Cruz <vitorluiz15012006@gmail.com>
2023-04-29 14:52:46 -0300

    introduction of props

---
+++ react-js/src/components/ShowUserName.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ShowUserName(props) {
+    return (
+        <div>
+            <h2>O nome do usuário: {props.name}</h2>
+        </div>
+    )
+}
--- react-js/src/pages/index.js
+++ react-js/src/pages/index.js
@@ -1,11 +1,13 @@
 import ListRender from '@/components/ListRender'
 import ConditionalRender from '@/components/ConditionalRender'
+import ShowUserName from '@/components/ShowUserName'
 
 export default function Home() {
   return (
     <>
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name="Vitão" />
     </>
   )
 }
