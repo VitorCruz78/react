63ca0b78acbdb6a1531ef62641cf92bf276a0b10
Vitor Luiz da Cruz <vitorluiz15012006@gmail.com>
2023-04-29 15:33:29 -0300

    reuse with loop

--- react-js/src/pages/index.js
+++ react-js/src/pages/index.js
@@ -4,6 +4,12 @@ import ShowUserName from '@/components/ShowUserName'
 import CarDetails from '@/components/CarDetails'
 
 export default function Home() {
+
+  const cars = [
+    { id: 1, brand: 'Polo', color: 'azul', km: 60000, newCar: true },
+    { id: 2, brand: 'Gol Quadrado', color: 'verde', km: 20000, newCar: false },
+    { id: 3, brand: 'Gol Bolinha', color: 'vermelho', km: 40000, newCar: true }
+  ]
   return (
     <>
       <ListRender />
@@ -15,6 +21,10 @@ export default function Home() {
       {/*reaproveitamento de componentes*/}
       <CarDetails brand="Picasso" km={10000} color="cinza" newCar={false} />
       <CarDetails brand="Ford" km={5000} color="amarelo" newCar={true} />
+      {/*loop em array de objetos*/}
+      {cars.map((detailsCar) => (
+        <CarDetails brand={detailsCar.brand} color={detailsCar.color} km={detailsCar.km} newCar={detailsCar.newCar} />
+      ))}
     </>
   )
 }
