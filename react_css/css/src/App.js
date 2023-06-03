b5a77ac729353732c1c0c0b5782cfbffe449e47f
Vitor Luiz da Cruz <vitorluiz15012006@gmail.com>
2023-06-03 14:16:13 -0300

    Estilos inline css com React

--- react_css/css/src/App.js
+++ react_css/css/src/App.js
@@ -8,6 +8,10 @@ function App() {
       <h1>React com CSS</h1>
       {/* CSS de componente */}
       <MyComponent />
+      {/* inline CSS */}
+      <p style={{ color: 'red', padding: "25px", borderTop: "2px solid red" }}>Este elemento foi estilizado de forma inline.</p>
+      {/* Inline CSS dinâmico */}
+      <p style={1 > 10 ? { color: "red" } : { color: "green" }}>Hello, World!</p>
     </div>
   );
 }
