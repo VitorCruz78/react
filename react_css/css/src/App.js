c42edddba57878753fca4ff28da127ef5e3bc308
Vitor Luiz da Cruz <vitorluiz15012006@gmail.com>
2023-06-03 13:32:29 -0300

    Iniciando React com CSS

---
+++ react_css/css/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyComponent from './components/MyComponents';
+
+function App() {
+  return (
+    <div className="App">
+      {/* CSS global */}
+      <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_css/css/src/components/MyComponents.js
@@ -0,0 +1,12 @@
+import "./MyComponents.css"
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>CSS de componente.</h1>
+            <p>Este é o parágrafo do componente.</p>
+        </div>
+    );
+}
+
+export default MyComponent;
