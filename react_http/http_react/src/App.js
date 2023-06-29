944a1562e1683078adafb75c9971822b60a72646
Vitor Luiz da Cruz <vitorluiz15012006@gmail.com>
2023-06-28 21:53:04 -0300

    Buscando e adicionando dados (GET, POST)

--- react_http/http_react/src/App.js
+++ react_http/http_react/src/App.js
@@ -1,9 +1,67 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products"
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  // 1 - resgatando dados
+  useEffect(() => {
+
+    async function getData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    getData()
+
+  }, [])
+
+  // 2 - add de produtos
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+  }
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => {
+          return <li key={product.id}>{product.name} - R$: {product.price}</li>
+        })}
+      </ul>
+      <div className='add-product'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome
+            <input type="text" value={name} name='name' onChange={(e) => setName(e.target.value)}></input>
+          </label>
+          <label>
+            Preço
+            <input type="number" value={price} name='price' onChange={(e) => setPrice(e.target.value)}></input>
+          </label>
+          <input type="submit" value="Criar"></input>
+        </form>
+      </div>
     </div>
   );
 }
